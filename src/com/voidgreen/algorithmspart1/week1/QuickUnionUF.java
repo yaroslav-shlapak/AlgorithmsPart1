cb39ce85d18879ad89b0b3f937dd59b955ec5437
Yaroslav <yaroslav.shlapak@gmail.com>
2015-09-13 18:18:39 +0300

    Save

---
+++ src/com/voidgreen/algorithmspart1/week1/QuickUnionUF.java
@@ -0,0 +1,43 @@
+package com.voidgreen.algorithmspart1.week1;
+
+/**
+ * Created by VOID on 12-09-15.
+ */
+public class QuickUnionUF {
+    private int[] id;
+    private int[] sz;
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for(int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while(i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+        if(i == j) return;
+        if(sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+}
