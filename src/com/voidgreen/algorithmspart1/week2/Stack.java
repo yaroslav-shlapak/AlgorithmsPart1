7c58f98118f8c0f042222ce3a46132b140dba6a9
Yaroslav <yaroslav.shlapak@gmail.com>
2015-09-19 19:11:32 +0300

    Iterator was implemented

--- src/com/voidgreen/algorithmspart1/week2/Stack.java
+++ src/com/voidgreen/algorithmspart1/week2/Stack.java
@@ -15,7 +15,21 @@ public class Stack<Item> implements Iterable<Item> {
     }
 
     private class ListIterator implements Iterator<Item> {
+        private Node current = first;
 
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 
     //linked-list node
